db7ce04dd95e3548db3c281e5015ed842c979533
mistimalaviya <mistiswaminarayan@gmail.com>
2025-01-08 23:30:45 +0530

    Day-6

---
+++ Day-6/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import UE from "./Components/UseEffect"
+
+
+function App() {
+  return (
+    <>
+        <UE></UE>
+    </>
+  )
+}
+
+export default App
